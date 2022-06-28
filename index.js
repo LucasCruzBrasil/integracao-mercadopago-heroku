b675bb03d7cd1d832a629039faa9d33c1e16655a
Lucas <lucasAngular@hotmail.com>
2022-06-27 21:59:39 -0300

    implementação pix, mercadopago

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ app.get("/pagar", async (req, res) => {
         },
         exeternal_reference: id
     }
- //blablagp
 
     try {
         var pagamentos = await mercadopago.preferences.create(dados);
@@ -45,11 +44,64 @@ app.get("/pagar", async (req, res) => {
 
 })
 
+// notificação mercado pago
 app.post('/not', (req, res) => {
     console.log(req.query);
     res.send('ok');
 })
 
+
+
+app.post("/pix", (req, res) => {
+    const requestBody = req.body;
+    const data = {
+      payment_method_id: "pix",
+      description: requestBody.description,
+      transaction_amount: Number(requestBody.transactionAmount),
+      payer: {
+        email: requestBody.payer.email,
+        first_name: requestBody.payer.firstName,
+        last_name: requestBody.payer.lastName,
+        identification: {
+          type: requestBody.payer.identification.type,
+          number: requestBody.payer.identification.number,
+        }
+      }
+    };
+  
+    mercadopago.payment.create(data)
+      .then(function(data) {
+          const { response } = data;
+  
+          res.status(201).json({
+            id: response.id,
+            status: response.status,
+            detail: response.status_detail,
+            qrCode: response.point_of_interaction.transaction_data.qr_code,
+            qrCodeBase64: response.point_of_interaction.transaction_data.qr_code_base64,
+          });
+      }).catch(function(error) {
+        console.log(error);
+        const { errorMessage, errorStatus }  = validateError(error);
+        res.status(errorStatus).json({ error_message: errorMessage });
+      });
+  });
+  
+  function validateError(error) {
+    let errorMessage = 'Unknown error cause';
+    let errorStatus = 400;
+  
+    if(error.cause) {
+      const sdkErrorMessage = error.cause[0].description;
+      errorMessage = sdkErrorMessage || errorMessage;
+  
+      const sdkErrorStatus = error.status;
+      errorStatus = sdkErrorStatus || errorStatus;
+    }
+  
+    return { errorMessage, errorStatus };
+  }
+  
 app.listen(port, (req, res) => {
     console.log('servidor rodando');
 })
