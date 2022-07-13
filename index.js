5f99191f69edbd10ed3c8945d5952f7eccab5754
Lucas <lucasAngular@hotmail.com>
2022-07-13 20:47:52 -0300

    integracao

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mercadopago = require('mercadopago');
 //huj
 var bodyParser = require('body-parser');
 const app = express();
+var cors = require('cors');
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //app.use(bodyParser.json());
@@ -10,7 +12,20 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000
 
- //teatando 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+      'Access-Control-Allow-Header',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).send({});
+  }
+  next();
+}); 
+
   mercadopago.configure({
   sandbox: false,
   access_token: "APP_USR-4017170961404208-062211-9e5c5a7aa6923eb3f8d773e18f31a219-273449421",
