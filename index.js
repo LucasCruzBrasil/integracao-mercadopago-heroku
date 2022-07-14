07c52d112fef3eb5312fe288bf2dfdaa87a33949
Lucas <lucasAngular@hotmail.com>
2022-07-13 21:21:11 -0300

    INTEGRACAO MERCADO-PAGO

--- index.js
+++ index.js
@@ -3,28 +3,14 @@ const mercadopago = require('mercadopago');
 //huj
 var bodyParser = require('body-parser');
 const app = express();
-/* var cors = require('cors');
-app.use(cors()); */
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //app.use(bodyParser.json());
 
 
 const port = process.env.PORT || 3000
-/* 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-      'Access-Control-Allow-Header',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
 
-  if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).send({});
-  }
-  next();
-});  */
 
   mercadopago.configure({
   sandbox: false,
