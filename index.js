ad0bf37051bd211e784d94670c23091eb197d3ba
Lucas <lucasAngular@hotmail.com>
2022-07-26 09:44:27 -0300

    notificacao de pagamento

--- index.js
+++ index.js
@@ -80,10 +80,7 @@ app.post('/not', (req, res) => {
       mercadopago.payment.search({
         qs: filtro
       }).then(data => {
-        
-        res.render('payment-search/search-result', {
-          result: data
-        });
+       var pagamento = data.body.results[0];
       
        if(pagamento != undefined){
        
