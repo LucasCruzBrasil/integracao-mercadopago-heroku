ef2e077451fde43469bcf810a0cda904a0339eee
Lucas <lucasAngular@hotmail.com>
2022-08-03 12:16:29 -0300

    instalando mysql no heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mercadopago = require('mercadopago');
-//huj
+const mysql = require('./mysql').pool
 var bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
