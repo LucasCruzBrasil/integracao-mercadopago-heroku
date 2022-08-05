4f1d5512c1e6a6494eeb5b5426547c2e285097bb
Lucas <lucasAngular@hotmail.com>
2022-08-05 09:41:51 -0300

    instalando mysql no heroku

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.post('/not', (req, res) => {
       console.log(date_created)
       console.log(date_approved)
 
-      if (pagamento == "pending" | pagamento == "cancelled") {
+      if (pagamento == "pending" || pagamento == "cancelled") {
         console.log('ainda não pagou');
 
       } else {
