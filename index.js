38fddd7302f27042bb637ddb93ed7af4c6400cfd
Lucas <lucasAngular@hotmail.com>
2022-08-06 09:20:39 -0300

    persistencia de dados de pagamento

--- index.js
+++ index.js
@@ -95,22 +95,15 @@ app.post('/not', (req, res) => {
 
       } else {
         console.log('Pagou')
+        
         mysql.getConnection((error, conn) => {
-          conn.query('INSERT INTO pagamentos(id_pagamento, transaction_amount, status_pagamento, description_pagamento, date_created, date_approved)VALUES(?,?,?,?,?,?)',
+        var sql = conn.query('INSERT INTO pagamentos(id_pagamento, transaction_amount, status_pagamento, description_pagamento, date_created, date_approved)VALUES(?,?,?,?,?,?)',
             [id, transaction_amount, pagamento, description_pagamento, date_created, date_approved],
-
-            (error, resultado, field) => {
-              conn.release();
-              if (error) {
-                return res.status(500).send({
-                  error: error,
-                  response: null
-                });
-              }
-              return res.status(201).send({
-                mensagem: "pago com sucesso!!",
-              })
+            (sql, function (err, result) {
+              if (err) throw err;
+              console.log("1 record inserted");
             })
+            )
         })
       }
     }).catch(err => {
